4e943966c512a714365ddef7263a952ae1a9458a
Terrence Cummings <terrence.cummings@gmail.com>
2020-03-20 17:58:58 -0500

    clean up

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,44 +2,29 @@
 #03-Python Honework
 #PyPoll exercise
 
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
 import os
-
-# Module for reading CSV files
 import csv
 csvpath = os.path.join('election_data.csv')
 with open(csvpath, newline='') as csvfile:
-
-# CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-#    print(csvreader)
-
-# Read the header row first (skip this step if there is now header)
+# Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-#    print(f"CSV Header: {csv_header}")
-
-# Read each row of data after the header
 #Initialize counter and tracker variables to null
     num_votes = 0
-  
 # candidate dictionary in form {name: [votes, pct]}
     candidate_dict = {}
-
 #for each vote
     for row in csvreader:
         vote_for = row[2]
         if vote_for not in candidate_dict:
             candidate_dict[vote_for] = [1, 0]
         else:
-            candidate_dict[vote_for] = [candidate_dict[vote_for][0]+1, 0]
-            
+            candidate_dict[vote_for] = [candidate_dict[vote_for][0]+1, 0]  
         num_votes = num_votes+1
-    
+    #calcute the pct of the vote for each candidate
     for candidate in candidate_dict:
         pct_vote = candidate_dict[candidate][0]/num_votes
         candidate_dict[candidate][1]=pct_vote
-
 #print table to terminal and file
     f= open("PyPoll.txt", "w+")
     print("Election Results")
@@ -69,4 +54,3 @@ with open(csvpath, newline='') as csvfile:
     print("---------------------------------")
     f.write("---------------------------------\r\n")
     f.close()
-
